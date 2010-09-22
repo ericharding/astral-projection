23c3ca635dba462b75fdf3e6225784df49b3ef24
Eric Harding <Eric Harding@localhost>
2010-09-22 00:19:04 -0400

    Settled on a set of classes for saving files

---
+++ AstralPlane/Map.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Astral.Plane
+{
+    /*
+     * Responsible for loading, saving and holding map tile data
+     */
+    public class Map
+    {
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes an empty Map
+        /// </summary>
+        public Map()
+        {
+            // empty map!
+        }
+        /// <summary>
+        /// Loads map data from the specified file
+        /// </summary>
+        /// <param name="fileName">A file conforming to the AstralMap spec</param>
+        public Map(string fileName)
+        {
+            Load(fileName);
+        }
+
+        #endregion
+
+        #region public Interface
+
+        /// <summary>
+        /// Add a reference to another map.  
+        /// This map will use the other map to resolve Tile references
+        /// </summary>
+        /// <param name="includedMap"></param>
+        public void AddReference(Map includedMap)
+        {
+        }
+
+        /// <summary>
+        /// Save the map to a file.
+        /// </summary>
+        /// <param name="full">If true the resulting file will include all necessary tiles and be completely standalone</param>
+        public void Save(bool standalone, string filename)
+        {
+
+        }
+
+        #endregion
+
+
+        #region private
+
+        private void Load(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        #endregion
+
+    }
+}
---
+++ AstralPlane/Tile.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Xml.Linq;
+
+namespace Astral.Plane
+{
+    public enum Rotation
+    {
+        Zero = 0,
+        CW90 = 90,
+        CW180 = 180,
+        CW270 = 270
+    }
+
+    public class Tile
+    {
+        internal Tile(TileFactory source)
+        {
+        }
+
+        public Point Location { get; set; }
+        public int Layer { get; set; }
+        public int Rotation { get; set; }
+   
+        internal XNode ToXML()
+        {
+            return new XElement("Tile",
+                new XAttribute("Location.X", this.Location.X),
+                new XAttribute("Location.Y", this.Location.Y),
+                new XAttribute("Layer", this.Layer),
+                new XAttribute("Rotation", this.Rotation));
+        }
+
+        internal void FromXML(XElement element)
+        {
+            throw new NotImplementedException();
+        }
+        
+    }
+}
