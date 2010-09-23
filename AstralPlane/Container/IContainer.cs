298e42958bb0f0ea926334b8330856ba91c874b6
Eric Harding <Eric Harding@localhost>
2010-09-23 14:17:54 -0400

    Created ZipFileContainer for internal use by AstralPlane. Also a basic FilesystemContainer implementation for debugging/comparison. I'm thinking of exposing a debug-only flag on Map to have it use the FilesystemContainer instead of the ZipFileContainer as a diagnostic.

---
+++ AstralPlane/Container/IContainer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Astral.Plane.Container
+{
+    interface IContainer : IDisposable
+    {
+        bool ContainsFile(string filename);
+        Stream GetFileStream(string filename, bool create = true);
+    }
+}
