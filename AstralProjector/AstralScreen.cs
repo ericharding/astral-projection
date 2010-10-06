2706e02b63e868dd656c5fd089901ba633d919b8
Eric Harding <Eric Harding@localhost>
2010-10-06 18:00:52 -0400

    screwing around

---
+++ AstralProjector/AstralScreen.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows;
+
+namespace Astral.Projector
+{
+    class AstralScreen : Control
+    {
+        protected override void OnRender(DrawingContext dc)
+        {
+            dc.PushOpacity(0.5);
+            
+            base.OnRender(dc);
+        }
+    }
+
+
+    static class DrawingContextHelper
+    {
+        public static void DrawHex(this DrawingContext dc)
+        {
+
+        }
+    }
+}
