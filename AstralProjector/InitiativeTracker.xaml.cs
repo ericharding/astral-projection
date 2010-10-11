47144937b6911e7321b59dc8afd3229737716271
Eric Harding <Eric Harding@localhost>
2010-10-10 23:29:18 -0400

    progress on InitiativeTracker

--- AstralProjector/InitiativeTracker.xaml.cs
+++ AstralProjector/InitiativeTracker.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Astral.Projector.Initiative;
 
 namespace Astral.Projector
 {
@@ -19,9 +20,27 @@ namespace Astral.Projector
     /// </summary>
     public partial class InitiativeTracker : UserControl
     {
+        InitiativeManager _unitInitiative;
+
         public InitiativeTracker()
         {
             InitializeComponent();
+
+            this.Loaded += new RoutedEventHandler(InitiativeTracker_Loaded);
         }
+
+        void InitiativeTracker_Loaded(object sender, RoutedEventArgs e)
+        {
+            _unitInitiative = new InitiativeManager(5,
+                new UnitInitiative() { Name = "Joe", MaxHealth = 30, CurrentHealth = 30, Initiative = 10, TeamImage = "Images/gold.ico" },
+                new UnitInitiative() { Name = "Mr. Stinky", MaxHealth = 30, CurrentHealth = 30, Initiative = 10, TeamImage = "/Images/green.ico" },
+                new UnitInitiative() { Name = "Zombie 2", MaxHealth = 30, CurrentHealth = 30, Initiative = 10, TeamImage = "/Images/green.ico" });
+
+            _initiativeList.ItemsSource = _unitInitiative;
+            this.DataContext = _initiativeList;
+        }
+
+
     }
+
 }
