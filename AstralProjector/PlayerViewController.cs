2b83be4f214f80cd9c0eafdf14990ffd745ef371
Eric Harding <Eric Harding@localhost>
2010-11-29 17:40:49 -0500

    api work

---
+++ AstralProjector/PlayerViewController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace Astral.Projector
+{
+    // todo: extract interface and make this remoteable
+    public class PlayerViewController
+    {
+        public void SetVisible(bool isVisible)
+        {
+        }
+
+        public void LoadMap(string mapPath)
+        {
+        }
+
+
+        public void DisplayEffect(string effect)
+        {
+        }
+
+        public void SetZoomLevel(int zoom)
+        {
+        }
+
+        public void UpdateMapPosition(Point newPosition)
+        {
+        }
+
+        //
+        // Fog of War methods
+        //
+
+        // todo: handle different zoom levels
+        public void UpdateFogAt(Point position, bool addFog)
+        {
+            
+        }
+    }
+}
