a423c0fc966db497c7e3fc087b5879225c27dd13
Eric Harding <Eric Harding@localhost>
2011-01-14 00:14:48 -0500

    Fix for crash on import if the prev directory doesn't exit.

---
+++ AstralPlane/Utility/AnonymousDisposable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Astral.Plane.Utility
+{
+    internal class AnonymousDisposable : IDisposable
+    {
+        private Action _dispose;
+        public AnonymousDisposable(Action dispose)
+        {
+            _dispose = dispose;
+        }
+
+        public void Dispose()
+        {
+            if (_dispose != null)
+                _dispose();
+        }
+    }
+}
