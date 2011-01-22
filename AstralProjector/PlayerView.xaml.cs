9b7c412fded61f0216ef6ff6700b3ac45d8d2d06
Ross Ivins <rivins@gmail.com>
2011-01-21 20:16:25 -0500

    adjust snap-to-grid slightly

---
+++ AstralProjector/PlayerView.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using TileMap;
+
+
+namespace Astral.Projector
+{
+    /// <summary>
+    /// Interaction logic for PlayerView.xaml
+    /// </summary>
+    public partial class PlayerView : Window
+    {
+        public PlayerView()
+        {
+            InitializeComponent();
+        }
+
+        public void ShowImage(string image)
+        {
+            BitmapImage src = new BitmapImage();
+            src.BeginInit();
+            src.StreamSource = File.OpenRead(image);
+            src.EndInit();
+            _imageViewer.Source = src;
+
+            Storyboard sb = this.Resources["ImageIn"] as Storyboard;
+            this.BeginStoryboard(sb);
+        }
+
+        public void HideImage()
+        {
+            Storyboard sb = this.Resources["ImageOut"] as Storyboard;
+            this.BeginStoryboard(sb);
+        }
+
+        public MapPane MapView { get { return _mapView; } }
+        public FogOfWar Fog { get { return _fog; } }
+
+    }
+}
