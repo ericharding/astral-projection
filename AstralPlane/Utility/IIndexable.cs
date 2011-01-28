b5571073e8004a9dfc58035d4f3e884ed644b564
Eric Harding <Eric Harding@localhost>
2011-01-27 21:27:53 -0500

    forgot to add file

---
+++ AstralPlane/Utility/IIndexable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Astral.Plane.Utility
+{
+    public interface IIndexable<T1, T2>
+    {
+        T2 this[T1 index] { get; set; }
+    }
+}
