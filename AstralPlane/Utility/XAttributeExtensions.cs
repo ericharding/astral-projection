485f446a1e168f42ce95383e75dba3e30d3802a8
Eric Harding <Eric Harding@localhost>
2011-01-29 21:07:36 -0500

    Additional rows for additional layers makes layout happy.

---
+++ AstralPlane/Utility/XAttributeExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Astral.Plane.Utility
+{
+    static class XAttributeExtensions
+    {
+        public static T Parse<T>(this XAttribute attribute, Func<string, T> parse)
+        {
+            if (attribute != null)
+            {
+                return parse(attribute.Value);
+            }
+            return default(T);
+        }
+    }
+}
---
+++ AstralProjector/PlayerView.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using TileMap;
+using System.Windows.Controls;
+
+
+namespace Astral.Projector
+{
+    /// <summary>
+    /// Interaction logic for PlayerView.xaml
+    /// </summary>
+    public partial class PlayerView : Window
+    {
+        public PlayerView()
+        {
+            InitializeComponent();
+        }
+
+        public void ShowImage(string image)
+        {
+            BitmapImage src = new BitmapImage();
+            src.BeginInit();
+            src.StreamSource = File.OpenRead(image);
+            src.EndInit();
+            _imageViewer.Source = src;
+
+            Storyboard sb = this.Resources["ImageIn"] as Storyboard;
+            this.BeginStoryboard(sb);
+        }
+
+        public void HideImage()
+        {
+            Storyboard sb = this.Resources["ImageOut"] as Storyboard;
+            this.BeginStoryboard(sb);
+        }
+
+        public MapPane MapView { get { return _mapView; } }
+        public FogOfWar Fog { get { return _fog; } }
+        public UIElementCollection Effects { get { return _effectContainer.Children; } }
+
+    }
+}
