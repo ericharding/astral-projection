c12ec64aa57df33610293e136021f6281b3ae406
Ross Ivins <rivins@gmail.com>
2011-03-12 01:35:51 -0500

    use animations in ImageDrifter

--- AstralProjector/Effects/ImageDrifter.cs
+++ AstralProjector/Effects/ImageDrifter.cs
@@ -2,16 +2,22 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
 using System.Windows.Threading;
 
 namespace Astral.Projector.Effects
 {
     public class ImageDrifter : Control, IDisposable
     {
-        int _offsetX = 0;
-        int _offsetY = 0;
-        DispatcherTimer _dt;
+        int _deltaX = 1;
+        int _deltaY = 1;
+        int _fps = 30;
+        int _width, _height;
         ImageBrush _imageBrush;
+        ImageSource _imageSource;
+        RectAnimation _rectAnim;
+        bool _loaded = false;
 
         static ImageDrifter()
         {
@@ -21,49 +27,74 @@ namespace Astral.Projector.Effects
         public ImageDrifter()
         {
             this.Loaded += new RoutedEventHandler(ImageDrifter_Loaded);
-            this.DeltaX = 1;
-            this.DeltaY = 1;
         }
 
         void ImageDrifter_Loaded(object sender, RoutedEventArgs e)
         {
-            _dt = new DispatcherTimer();
-            _dt.Interval = TimeSpan.FromMilliseconds(33);
-            _dt.Tick += new EventHandler(_dt_Tick);
-            _dt.Start();
-        }
-
-        void _dt_Tick(object sender, EventArgs e)
-        {
-            _offsetY += this.DeltaY;
-            _offsetX += this.DeltaX;
-            _imageBrush.Viewport = new Rect(_offsetX, _offsetY, 800, 600);
+            _loaded = true;
+            ResetAnimation();
         }
 
         public override void OnApplyTemplate()
         {
             base.OnApplyTemplate();
             _imageBrush = (ImageBrush)this.GetTemplateChild("_clouds");
+
+            ResetAnimation();
+        }
+
+        private void ResetAnimation()
+        {
+            if (!_loaded)
+                return;
+
             _imageBrush.ImageSource = this.ImageSource;
+            _width = ((BitmapSource)this.ImageSource).PixelWidth;
+            _height = ((BitmapSource)this.ImageSource).PixelHeight;
+
+            int x = _deltaX * _width;
+            int y = _deltaY * _height;
+
+            Vector vTotal = new Vector(x, y);
+            Vector vStep = new Vector(_deltaX, _deltaY);
+
+            Duration dur = new Duration(TimeSpan.FromSeconds((1f / _fps) * (vTotal.Length / vStep.Length)));
+
+            Rect from = new Rect(0, 0, _width, _height);
+            Rect to = new Rect(x, y, _width, _height);
+
+            _rectAnim = new RectAnimation(from, to, dur);
+            _rectAnim.RepeatBehavior = RepeatBehavior.Forever;
+
+            _imageBrush.BeginAnimation(ImageBrush.ViewportProperty, _rectAnim);
         }
 
         public void Dispose()
         {
-            _dt.Stop();
         }
 
-        public int DeltaX { get; set; }
-        public int DeltaY { get; set; }
+        public int DeltaX
+        {
+            get { return _deltaX; }
+            set { _deltaX = value; ResetAnimation(); }
+        }
+
+        public int DeltaY
+        {
+            get { return _deltaY; }
+            set { _deltaY = value; ResetAnimation(); }
+        }
+
+        public int FPS
+        {
+            get { return _fps; }
+            set { _fps = value; ResetAnimation(); }
+        }
 
         public ImageSource ImageSource
         {
-            get { return (ImageSource)GetValue(ImageSourceProperty); }
-            set { SetValue(ImageSourceProperty, value); }
+            get { return _imageSource; }
+            set { _imageSource = value; ResetAnimation(); }
         }
-
-        public static readonly DependencyProperty ImageSourceProperty =
-            DependencyProperty.Register("ImageSource", typeof(ImageSource), typeof(ImageDrifter), new UIPropertyMetadata(null));
-
-        
     }
 }
